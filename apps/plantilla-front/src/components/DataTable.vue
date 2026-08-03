0b7c3c81167b33eaf2f62cf815564bed246d87d5
irwin <ijesperanza@innovacion.gob.sv>
2026-08-03 14:24:14 -0600

    cambios

---
+++ apps/plantilla-front/src/components/DataTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :no-data-text="noData"
+      hide-default-footer
+      
+    ></v-data-table>
+    <Pagination :total-page="totalPage" :page="page" @update:page="onPageChange"></Pagination>
+  </div>
+</template>
+<script setup>
+import Pagination from "./Pagination.vue";
+import { defineProps, defineEmits } from "vue";
+
+const emit = defineEmits(['update:page']);
+
+const onPageChange = (newPage) => {
+  emit('update:page', newPage);
+};
+
+const props = defineProps({
+  headers: {
+    type: Array,
+    default: [],
+  },
+  items: {
+    type: Array,
+    default: [],
+  },
+  noData: {
+    type: String,
+    default: "No hay datos",
+  },
+  totalPage:{
+    type:Number,
+    default:1
+  },
+  page:{
+    type:Number,
+    default:1
+  }
+});
+</script>
---
+++ apps/plantilla-front/src/components/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-pagination :length="totalPage" v-model="paginas" @update:modelValue="onPageChange"></v-pagination>
+</template>
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+    page:{
+        type:Number,
+        default:1
+    },
+    totalPage:{
+        type:Number,
+        default:1
+    }
+})
+
+const emit = defineEmits(['update:page']);
+
+const paginas = ref(props.page);
+
+watch(() => props.page, (newPage) => {
+    paginas.value = newPage;
+});
+
+const onPageChange = (newPage) => {
+    emit('update:page', newPage);
+};
+</script>
